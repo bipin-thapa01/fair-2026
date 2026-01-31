d698349a8f6d806e1e7efa21299f03cedc73295c
Safal-Shrestha <shressafal05@gmail.com>
2026-01-31 20:40:21 +0545

    Initialization of backend server

---
+++ backend/src/main/java/com/app/bridgeQuality/BridgeGuardApplication.java
@@ -0,0 +1,13 @@
+package com.app.bridgeQuality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeGuardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/bridgeQuality/BridgeGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bridgeQuality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
