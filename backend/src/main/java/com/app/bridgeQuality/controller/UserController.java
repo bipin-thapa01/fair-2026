a7901a5c689244476668fa5cc25c5f560638e393
Safal-Shrestha <shressafal05@gmail.com>
2026-02-02 20:11:51 +0545

    unused import removed

--- backend/src/main/java/com/app/bridgeQuality/controller/UserController.java
+++ backend/src/main/java/com/app/bridgeQuality/controller/UserController.java
@@ -1,18 +1,17 @@
 package com.app.bridgeQuality.controller;
 
-import com.app.bridgeQuality.entity.User;
-import com.app.bridgeQuality.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import com.app.bridgeQuality.entity.User;
+import com.app.bridgeQuality.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/user")
--- backend/src/main/java/com/app/bridgeQuality/repository/BridgeHealthLogRepository.java
+++ backend/src/main/java/com/app/bridgeQuality/repository/BridgeHealthLogRepository.java
@@ -1,11 +1,10 @@
 package com.app.bridgeQuality.repository;
 
-import com.app.bridgeQuality.entity.Bridge;
-import com.app.bridgeQuality.entity.BridgeHealthLog;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-import java.util.UUID;
+import com.app.bridgeQuality.entity.BridgeHealthLog;
 
 public interface BridgeHealthLogRepository extends JpaRepository<BridgeHealthLog, UUID> {
 }
